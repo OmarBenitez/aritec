8bd834d15bee4f2cc032a628cfc3a858cb2dac4e
Omar Benitez <oabe2911@gmail.com>
2022-12-02 10:00:18 -0600

    change chart

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.listen(port, function () {
 app.get('/api/getCaudalKPIs', (req, res) => {
 
     var coll = database.collection('caudal');
-    coll.find({ }).sort({timestamp: -1}).limit(10).toArray(function (e, d) {
+    coll.find({ }).sort({timestamp: -1}).limit(20).toArray(function (e, d) {
 
         if(e) {
             res.send(500,e);
@@ -87,7 +87,7 @@ app.get('/reports/getCaudalReport', (req, res) => {
     sheet.getCell('B3').value = "Promedio de Caudal";
 
     var coll = database.collection('caudal');
-    coll.find({ }).sort({timestamp: -1}).limit(10).toArray(function (e, d) {
+    coll.find({ }).sort({timestamp: -1}).toArray(function (e, d) {
         if(e) {
             res.send(500,e);
         } else {
@@ -110,5 +110,50 @@ app.get('/reports/getCaudalReport', (req, res) => {
     });
 
 ;
+
+})
+
+app.get('/api/seedCaudalData', (req, res) => {
+    const coll = database.collection('caudal');
+
+    const data = [
+        {QT:15.3900139940599,timestamp: new Date('2022-11-02')},
+        {QT:0,timestamp: new Date('2022-11-03')},
+        {QT:15.0017006772922,timestamp: new Date('2022-11-04')},
+        {QT:0,timestamp: new Date('2022-11-05')},
+        {QT:14.9281564264905,timestamp: new Date('2022-11-06')},
+        {QT:0,timestamp: new Date('2022-11-07')},
+        {QT:15.3380481722229,timestamp: new Date('2022-11-08')},
+        {QT:0,timestamp: new Date('2022-11-09')},
+        {QT:14.2734669074149,timestamp: new Date('2022-11-10')},
+        {QT:0,timestamp: new Date('2022-11-11')},
+        {QT:15.4272683880192,timestamp: new Date('2022-11-12')},
+        {QT:0,timestamp: new Date('2022-11-13')},
+        {QT:14.1643710843173,timestamp: new Date('2022-11-14')},
+        {QT:0,timestamp: new Date('2022-11-15')},
+        {QT:14.8163842593365,timestamp: new Date('2022-11-16')},
+        {QT:0,timestamp: new Date('2022-11-17')},
+        {QT:15.33483216836,timestamp: new Date('2022-11-18')},
+        {QT:0,timestamp: new Date('2022-11-19')},
+        {QT:15.0260662342507,timestamp: new Date('2022-11-20')},
+        {QT:0,timestamp: new Date('2022-11-21')},
+        {QT:15.0910920851111,timestamp: new Date('2022-11-22')},
+        {QT:0,timestamp: new Date('2022-11-23')},
+        {QT:9.73375484488466,timestamp: new Date('2022-11-24')},
+        {QT:0,timestamp: new Date('2022-11-25')},
+        {QT:10.9702172616423,timestamp: new Date('2022-11-26')},
+        {QT:0,timestamp: new Date('2022-11-27')},
+        {QT:9.65339270592293,timestamp: new Date('2022-11-28')},
+        {QT:0,timestamp: new Date('2022-11-29')},
+        {QT:10.0492624653479,timestamp: new Date('2022-11-30')},
+        {QT:0,timestamp: new Date('2022-11-31')},
+        {QT:10.0005441442769,timestamp: new Date('2022-11-32')},
+    ];
+
+    for(var d of data) {
+        coll.insertOne({...d, year: d.timestamp.getFullYear(), month: d.timestamp.getMonth()+1, day: d.timestamp.getDate()})
+    }
+
+    res.send(200,data);
 
 })
--- public/scripts/ng-app/app.js
+++ public/scripts/ng-app/app.js
@@ -86,6 +86,9 @@ app.controller('indexCtrl', ['$http', '$scope', 'socket', ($http, $scope, socket
         return ["hsl(", hue, ",100%,50%)"].join("");
     }
 
+    const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
+    const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+
     const buildChart = (data) => {
 
         var ctx2 = document.getElementById("chart-line").getContext("2d");
@@ -116,8 +119,7 @@ app.controller('indexCtrl', ['$http', '$scope', 'socket', ($http, $scope, socket
                     backgroundColor: gradientStroke1,
                     fill: true,
                     data: data.map(_ => _.q1),
-                    maxBarThickness: 6
-
+                    //maxBarThickness: 6,
                 }
                 ],
             },
